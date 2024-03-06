b259058c2a990b1c2c625259d10e3f69c89a8cf6
suragch <suragch@suragch.dev>
2024-03-06 11:18:45 +0800

    initial commit

---
+++ bin/server.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+final _router = Router()..get('/', _rootHandler);
+
+Response _rootHandler(Request req) {
+  final keyEnv = Platform.environment['KEY'];
+  const keyString = String.fromEnvironment('KEY');
+  print('keyEnv: $keyEnv');
+  print('keyString: $keyString');
+  return Response.ok('keyEnv: $keyEnv\nkeyString: $keyString');
+}
+
+void main(List<String> args) async {
+  final ip = InternetAddress.anyIPv4;
+  final handler =
+      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
