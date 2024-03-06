961af874e8af7ac9b40e617ec99433ce5b304963
suragch <suragch@suragch.dev>
2024-03-06 19:58:11 +0800

    add /env debugging route

--- bin/server.dart
+++ bin/server.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-final _router = Router()..get('/', _rootHandler);
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/env', _envHandler);
 
 Response _rootHandler(Request req) {
   final keyEnv = Platform.environment['KEY'];
@@ -12,6 +15,14 @@ Response _rootHandler(Request req) {
   print('keyEnv: $keyEnv');
   print('keyString: $keyString');
   return Response.ok('keyEnv: $keyEnv\nkeyString: $keyString');
+}
+
+Response _envHandler(Request req) {
+  final content = JsonEncoder.withIndent('  ').convert(Platform.environment);
+  return Response.ok(
+    content,
+    headers: {'content-type': 'application/json'},
+  );
 }
 
 void main(List<String> args) async {
